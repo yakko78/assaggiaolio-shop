0152d32f92f080a744798b6f611f6018f9a76c8f
Enrico Querci <chico@MacBook-Air-di-Enrico.local>
2016-06-20 14:48:53 +0200

    - add model LineItem - add model Cart - add basic ajax Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,11 +27,12 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_url, notice: 'Line item was successfully created.' }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
