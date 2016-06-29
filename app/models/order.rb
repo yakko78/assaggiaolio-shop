d0b0d41715d23e4b20ed7172dfb87a0d5a585bf3
Enrico Querci <chico@MacBook-Air-di-Enrico.local>
2016-06-29 12:44:13 +0200

    log stuff out of Order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -125,6 +125,8 @@ class Order < ActiveRecord::Base
       params[quantita_key] = quantita_value
     end
 
+    put "PARAMS: #{params}"
+
     result = Net::HTTP.post_form(URI.parse(url), params)
 
     result
