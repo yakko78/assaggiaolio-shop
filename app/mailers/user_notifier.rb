e3411649027303041a529fb93f1375b72d04667c
Enrico Querci <chico@MacBook-Air-di-Enrico.local>
2016-08-01 12:08:35 +0200

    add translations for order error message

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -3,7 +3,7 @@ class UserNotifier < ApplicationMailer
 
   def send_receipt_email(order)
     @order = order
-    mail(to: @order.billing_address.email, bcc: "info@yellowtulip.it", subject: t('email_receipt_title'))
+    mail(to: @order.billing_address.email, bcc: Rails.application.secrets.paypal_merchant_email, subject: t('email_receipt_title'))
   end
 
 end
